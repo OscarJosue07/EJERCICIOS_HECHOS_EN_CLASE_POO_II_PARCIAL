11380087d0da45d9d88ca64d1c43b7840356f756
Oscar Josué Pineda Dubon <ojpineda2@uth.hn>
2025-10-28 09:20:13 -0600

    Refactor Main.java to improve output structure

--- Recursividad/src/Main.java
+++ Recursividad/src/Main.java
@@ -2,19 +2,21 @@ import Clases.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        
+System.out.println("========== ECUACION SUMATORIA ==========");
 EcuacionSumatoria.ecuacionSumatoria();
 
-System.out.println("RECURSIVIDAD");
+System.out.println("========== RECURSIVIDAD ==========");
 Recursividad r = new Recursividad();
 r.ContarAtras(10);
 
+System.out.println("========== CICLO FOR ==========");
 CicloFor cf = new  CicloFor(0,100,3);
 cf.ForHechizo();
 
-CicloWhile cf = new CicloWhile(0,100,3);
-cf.WhileHechizo();
-
+System.out.println("========== CICLO WHILE ==========");        
+CicloWhile cw = new CicloWhile(0,100,3);
+cw.WhileHechizo();
 
     }
 }
